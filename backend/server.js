bafa2974364fdec818c4f8b8a17028270ee6d378
Sedef Cakmak <cakmaksedef@gmail.com>
2024-08-08 18:19:37 +0300

    added allowedOrigins array for CORS error

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,20 @@ import walletRouter from "./routes/wallet.js";
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
+const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 
 app.use("/api", walletRouter);
